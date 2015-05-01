4fcbdf79f1fce0a1be81548f2638b2b0d2ed3bec
Wang Xuerui <idontknw.wang@gmail.com>
2015-05-01 20:38:43 +0800

    oops, forgot to remove script building in build.sh

--- build.sh
+++ build.sh
@@ -21,15 +21,4 @@ fi
 $minimal_apt_get_install $deps
 
 
-# set up script
-sed_command="s/@ENABLE_EMAIL@/${enable_email}/;"
-sed_command="${sed_command} s/@ADMIN_EMAIL@/${admin_email}/;"
-sed_command="${sed_command} s#@SYNC_REMOTE@#${sync_remote}#;"
-sed_command="${sed_command} s#@MIRROR_PATH@#${mirror_path}#;"
-sed_command="${sed_command} s/@MIRROR_NAME@/${mirror_name}/;"
-
-sed_script "${sed_command}" mirror-sync.sh
-sed_script "${sed_command}" initial-sync.sh
-
-
 # vim:set ai et ts=4 sw=4 sts=4 fenc=utf-8:
